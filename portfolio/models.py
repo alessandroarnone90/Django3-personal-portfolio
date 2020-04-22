02ab150993367ec6d731b4dffc952e0458e99c6d
Alessandro Arnone <alessandroarnone90@gmail.com>
2020-04-22 14:27:32 +0200

    new projects

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title= models.CharField(max_length=10000)
-    description= models.CharField(max_length=10000)
+    description= models.TextField()
     image=models.ImageField(upload_to='portfolio/images/', blank=True)
     url=models.URLField(blank=True)
     technology= models.CharField(max_length=10000)
